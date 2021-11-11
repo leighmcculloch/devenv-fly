7b45483f719ff890b47c6d2408e00e41f7521c05
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-11-10 22:40:08 -0800

    Improvements

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 FLY_ORG?=personal
-FLY_APP?=leighdev
+FLY_APP?=flydev
 
 deploy: build
 	fly deploy -a $(FLY_APP)
@@ -9,7 +9,13 @@ build:
 
 create:
 	fly apps create --org $(FLY_ORG) $(FLY_APP)
-	fly scale vm -a $(FLY_APP) dedicated-cpu-8x
+	$(MAKE) size1
+
+size1:
+	fly scale vm -a $(FLY_APP) dedicated-cpu-8x --memory=16384
+
+size8:
+	fly scale vm -a $(FLY_APP) shared-cpu-1x --memory=2048
 
 destroy:
 	fly apps destroy -y $(FLY_APP)
--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ set -e
 echo "Starting tailscale daemon..."
 tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 echo "Starting tailscale up..."
-until tailscale up --hostname=leighdev
+until tailscale up --hostname=flydev
 do
     sleep 0.1
 done
@@ -15,12 +15,10 @@ mkdir -p /data/docker /etc/docker
 echo '{"data-root": "/data/docker", "ipv6": true, "fixed-cidr-v6": "2001:db8:1::/64"}' > /etc/docker/daemon.json
 service docker start
 sleep 2
-# set +e
 ip -6 route add 2001:db8:1::/64 dev docker0
 sysctl net.ipv6.conf.default.forwarding=1
 sysctl net.ipv6.conf.all.forwarding=1
 ip6tables -t nat -A POSTROUTING -s 2001:db8:1::/64 -j MASQUERADE
-# set -e
 
 echo "Configuring inotify for VSCode..."
 sysctl -w fs.inotify.max_user_watches=524288
